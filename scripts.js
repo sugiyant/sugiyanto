0f627a96ad8cb93cb1adc506a12f06c983c7cea0
sugiyanto <giyanto77@gmail.com>
2025-02-28 17:08:31 +0000

    menambah API Al Quran dan Hadits

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function fetchQuote() {
     // URL API untuk mendapatkan quotes dari Al-Quran dan Hadits
     const apiUrls = [
-        'https://api.alquran.cloud/v1/ayah/random',
+        'https://api.alquran.cloud/v1/ayah/random?translations=id.indonesian', // Menambahkan terjemahan Bahasa Indonesia
         'https://api.hadith.sutanlab.id/books/muslim?range=1-300'
     ];
 
@@ -16,18 +16,23 @@ function fetchQuote() {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            let quote, source;
+            let quote, source, translation = "";
 
             if (apiUrl.includes('alquran')) {
-                quote = data.data.text;
+                quote = data.data.text; // Teks Arab
+                translation = data.data.translations[0].text; // Terjemahan Bahasa Indonesia
                 source = `Al-Quran Surah ${data.data.surah.englishName} Ayah ${data.data.numberInSurah}`;
             } else if (apiUrl.includes('hadith')) {
                 const hadith = data.data.hadiths[Math.floor(Math.random() * data.data.hadiths.length)];
-                quote = hadith.arab;
+                quote = hadith.arab; // Teks Arab
+                translation = hadith.id; // Terjemahan Bahasa Indonesia
                 source = `Hadith Muslim Book ${hadith.bookNumber} Hadith ${hadith.hadithNumber}`;
             }
+            //gabungkan arab dan terjemahan
+            const combine = `<div style="text-align: right; font-size:1.2em; margin-bottom:10px">${quote}</div>
+            <div>${translation}</div>`;
 
-            document.getElementById('quote').innerText = quote;
+            document.getElementById('quote').innerHTML = combine;
             document.getElementById('source').innerText = source;
         })
         .catch(error => console.error('Error fetching quote:', error));
